87ab3295971676f6f06d49811b60e09bc2506e3b
nayaunglin910@gmail.com <nayaunglin910@gmail.com>
2024-03-20 21:22:30 +0630

    Finish Up Writing Content on Services and Contact Pages.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,6 +18,18 @@ export class AboutComponent {
         'At Golden Link, we are passionate about helping you achieve your Japanese language goals and opening doors to exciting opportunities in Japan. We are more than just a language school. We are your golden link to success in Japan.',
       title: '🙇‍♀️Welcome to ✨Golden Link Japanese & Other Languages Academy!',
     },
+    {
+      imageUrl: '/assets/images/subaru-seminar/two-senseis-collage.webp',
+      imageName: 'Sensei Emily',
+      content: "Mrs. Thin Htet Htet Aung and Mr. Thura Aung, the two founders of the Golden Link Japanese & Other Languages Academy have many years of experience with not only the education that Myanmar citizens can pursue in Japan but also about various job oppotunities as well. They work with various well-resource job agencies and honorable Japanese language schools in Japan. They have already helped many youths' dreams of continuing their future dreams in Japan become reality.",
+      title: 'Founders of Golden Link',
+    },
+    {
+      imageUrl: '/assets/images/subaru-seminar/raising-hands-students-senseis.webp',
+      imageName: 'Students and Senseis Raising Hands',
+      content: "We believe that any person who has a dream of building their future in Japan should be given a chance. We strike to give the best services and trainings to our students to become proud and respectable people not only for not their famalies but also for their country, Myanmar. We strike to help build the reputation of Myanmar citizens to be the absolute best as it can be in the foreign land.",
+      title: 'What We Believe In!',
+    }
   ];
   constructor() {}
 }
--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { AosFadeContentComponent } from '../aos-fade-content/aos-fade-content.component';
+import { aosContent } from '../aos-fade-content/types/aosContent.interface';
 
 @Component({
   selector: 'gl-our-services',
   standalone: true,
-  imports: [],
+  imports: [AosFadeContentComponent],
   templateUrl: './our-services.component.html',
   styleUrl: './our-services.component.css'
 })
 export class OurServicesComponent {
+  
+  aosContents: aosContent[] = [
+    {
+      imageUrl: '/assets/images/services/Various-Japanese-Language-Schools.webp',
+      imageName: 'Applying to Various Japanese Language School',
+      content:
+        "We primarily apply our students to Japanese language schools with best COE approval rates. Students may also choose to apply the schools they desire as well. We have various Japanese language schools that are working closely and openly with us.",
+      title: 'Applying to Various Japanese Language School!',
+    },
+    {
+      imageUrl: '/assets/images/job-agency-visit/students-job-agency-visit.webp',
+      imageName: 'High-Quality Japanese Language Lectures',
+      content:
+        "We offer a variety of Japanese language courses tailored to different proficiency levels and learning goals. Our experienced and dedicated teachers use innovative and effective methods to help you master all aspects of the language, from speaking and listening to reading, writing and being always prepared for the interviews.",
+      title: 'High-Quality Japanese Language Lectures!',
+    },
+    {
+      imageUrl: '/assets/images/job-students-group/job-students-photo_1.webp',
+      imageName: 'Help Students Find Tokutei and Work Visa Jobs',
+      content:
+        "We make sure our students willing to go to Japan with Tokutei jobs or work visa jobs find the suitable jobs they deserve. Not only we introduce them with job oppotunities, we also train them well so that they have a very high chance of passing the interview. We are proud to say that there are aleady many of our students who are currently now working in Japan!",
+      title: 'High-Quality Japanese Language Lectures!',
+    },
+  ];
 
+  constructor(){}
 }
